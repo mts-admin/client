b32a9c29e4df91cea051c10e8b873c1783163a4d
Vitalii Chornous <v.chornous@apiko.com>
2021-10-30 00:52:32 +0300

    updated min-height of auth layout

--- src/layouts/auth-layout/styled-components.js
+++ src/layouts/auth-layout/styled-components.js
@@ -21,7 +21,7 @@ export const Content = styled.main`
   display: flex;
   width: 85%;
   max-width: 1300px;
-  min-height: 85vh;
+  min-height: 90vh;
   padding: 24px;
   border-radius: ${({ theme }) => theme.borderRadius.medium};
   background-color: ${({ theme }) => theme.backgrounds.backgroundTransparent};
